10ecfa419e2f7f5278117f6370c0d735327e85b2
Arx <72176266+Arctic-Lab@users.noreply.github.com>
2022-04-25 15:01:50 +0800

    新站的自动登录脚本

--- me-login.py
+++ me-login.py
@@ -8,31 +8,27 @@ import time
 
 def work():
     # 请改成自己的账户名
-    _name = "用户名" 
+    _name = "usrname" 
     # 请改成自己的密码
-    _password = "密码"
+    _password = "password"
+    session = requests.session()
     cnt = 0
     while(1):
         if(cnt > 3):
             break
         cnt = cnt+1
-        time.sleep(random.randint(1, 600))
+        # time.sleep(random.randint(1, 600))
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        session = requests.session()
+        
         length1 = len(session.get("https://masiro.me/admin").content)
         url = "https://masiro.me/admin/auth/login"
-        # print(session.get(url).content)
-        # print(session.get(url).content, file=MYFILE)
         soup = BeautifulSoup(session.get(url).content, "html.parser", from_encoding="utf-8")
 
         links = soup.find_all('input', class_='csrf')
-        # print("all of links ", len(links))
         s = str(links[0])
-        print(s)
+        # print(s)
         begin = re.match(".*value=\"", s).end()
         end = re.match(".*", s).end()-3
-        # print(begin, " ", end)
-        # print( re.match(".*value=", s).end() )
         print("token = ", s[begin: end])
         token = s[begin: end]
         
@@ -43,13 +39,19 @@ def work():
             "remember" : "1",
             "_token" : str(token)
         }
-
         session.post("https://masiro.me/admin/auth/login", data=input_data)
+        session.get('https://masiro.me/admin/checkAnnouncement')
         length = len(session.get("https://masiro.me/admin").content)
-        if(length >= length1):
+
+        s = str(session.get('https://masiro.me/admin/userCenterShow').content, 'utf-8')
+        s = re.findall(r'\u8D44\u5386:.{4}', s)[0]
+        print(s)
+
+        if(length > length1):
             print("Accepted")
             break
         else:
             print("Fail to login")
+    
 
 work()
